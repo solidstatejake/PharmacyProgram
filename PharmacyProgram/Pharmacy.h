8085553927c3da402db0ed17fb22e9317077aa05
solidstatejake <jake.berg@sunydutchess.edu>
2017-11-24 14:54:00 -0500

    Massive amount of changes. Had gotten the password read-in working... now does not work again. Began adding the switch case that will designate which decision the pharmacist would like to make. Made it so if username/password not accepted, user is rerouted to reenter information.

--- PharmacyProgram/Pharmacy.h
+++ PharmacyProgram/Pharmacy.h
@@ -26,12 +26,12 @@ public:
 	~Pharmacy();
 		//Class destructor
 
-	void clearConsole();
+	void clearConsole() const;
 		//This function will clear the console as the user navigates from the program's capability
 		//to capability.
 		//Postcondition: The console will be cleared.
 
-	void introToUser();
+	void introToUser() const;
 		//Introduces user to the program.
 		//Postcondition: A paragraph is produces explaining the program to the user.
 
@@ -41,23 +41,32 @@ public:
 		//member variables username and password.
 	
 	
-	int verifyCredentials(string, string);
+	int verifyCredentials() const;
 		//This function will check the credentials extracted from the console in the function 
 		//grabPharmacistName against the files that contain acceptable usernames and passwords.
 		//If the credentials are appropriate, the function will permit access to the further depths 
 		//of the program.
 		//Postcondition: Administrative permissions are either granted or denied.
 
+	void displayPharmacistOptions();
+
+	void fillPrescription();
+
+	void updateStock();
+
 	void viewStock();
 		//This function will allow the user to access the file containing the information on 
 		//the status of the medications in stock.
 		//Postcondition: User is now able to manipulate the sequential data file "Stock.dat".
 
-	
-
+	void displayReceipts();
 
 	string username;
 	string password;
+
+private:
+	int pharmacistDecision; 
+	int continueOrNot;
 };
 
 
--- PharmacyProgram/PharmacyImp.cpp
+++ PharmacyProgram/PharmacyImp.cpp
@@ -12,7 +12,7 @@ Pharmacy::~Pharmacy()
 {
 }
 
-void Pharmacy::clearConsole()
+void Pharmacy::clearConsole() const
 {
 	//Makes sure that the screen is not cleared immediately and program will wait until they press enter
 	cin.ignore(1, ' \n');
@@ -32,38 +32,66 @@ void Pharmacy::clearConsole()
 }
 
 
-void Pharmacy::introToUser()
+void Pharmacy::introToUser() const
 {
 	cout << "PharmaTech Version 1.0" << endl;
 }
 
 
-void Pharmacy::grabUsername()//string &username, string &password)
+void Pharmacy::grabUsername()
 {
-	char characterInput = ' ';
+	//Making use of whitespace.
+	cout << endl;
 
+	//Creates carry variable to carry character input from console into the public member variable "password".
+	char transferChar = ' ';
+
+	//User is prompted for their username. That username is stored into the public member variable "username".
 	cout << "Please enter your username: ";
 	cin >> username;
 
+	//User is prompted for their password. That password is stored into the public member variable "password".
 	cout << "Please enter your password: ";
-	while (characterInput != 13)
+	
+	//13 in ASCII is "return carrige" colloquially known as enter/return. We say while user does not input enter
+	//allow inputs. If enter, end inputs.
+	while (transferChar != 13)
 	{
-		characterInput = _getch();
-		if (characterInput == 8)
+		//The char used to transfer individual character entries from the console to the public member variable
+		//password gains its char values by grabbing each individual char entered in console by user. Therefore
+		//password input is processed on a char-by-char basis.
+		transferChar = _getch();
+
+		//8 in ASCII is delete. If user hits delete key, the last character input will be deleted.
+		if (transferChar == 8)
 		{
+			//pop_back() will remove the last character input into the istream
 			password.pop_back();
-			cout << "\b";					//STILL NEEDS TO BE REVIEWED. ASTERISKS WILL NOT GO AWAY AFTER TYPED
-		}
-		else
+
+			cout << "\b";//STILL NEEDS TO BE REVIEWED. ASTERISKS WILL NOT GO AWAY AFTER TYPED
+
+		}//end of if
+		//If user does not press enter or delete, char grabbed by _getch() and put into transferChar is placed
+		//at the end of the password string. Since password string begins empty, password string is equivalent
+		//only to what is pushed onto the back of the password string right here.
+		else if(transferChar != 13 && transferChar != 8)
 		{
-			password.push_back(characterInput);
+			//push_back() will put what is being held in transfer char onto the end of the string variable password.
+			password.push_back(transferChar);
+
+			//Asterisk is output to simulate a hidden password.
 			cout << '*';
-		}
-	}
+			
+		}//end of else
+	}//end of while
+	
+	cout << endl;
 }
 
-int Pharmacy::verifyCredentials(string username, string password)
+int Pharmacy::verifyCredentials() const
 {
+
+	//cout << endl << username << password << endl << endl;
 	//Input file variables used to access the files containing usernames and passwords
 	ifstream usernameFile, passwordFile;
 
@@ -71,11 +99,11 @@ int Pharmacy::verifyCredentials(string username, string password)
 	//inputs as their username to check the validity of their entry. 
 	string compareCreds;
 
-	int usernameYesOrNo = 0,
+	bool usernameYesOrNo = 0,
 		passwordYesOrNo = 0;
 
-	
-	//Opens the file containing verified username information
+	//Opens the file containing verified username information. The period at the front is known as the
+	//"local path operator".
 	usernameFile.open(".\\UserInfo\\usernames.dat");
 	
 	//ERROR CHECK to make sure file opens
@@ -94,18 +122,18 @@ int Pharmacy::verifyCredentials(string username, string password)
 		//compareCreds string is compared to user-entered username. If two match, username is accepted. Else not accepted.
 		if (compareCreds == username)
 		{
-			usernameYesOrNo = 1;
-			return 1;
+			usernameYesOrNo = true;
+			break;
 		}
 		else
 		{
-			usernameYesOrNo = 0;
+			usernameYesOrNo = false;
 		}
 	}
 	
 	//The file containing the verified usernames is closed.
 	usernameFile.close();
-
+	
 	
 	//Opens the file containing verified password information
 	passwordFile.open(".\\UserInfo\\passwords.dat");
@@ -114,29 +142,36 @@ int Pharmacy::verifyCredentials(string username, string password)
 	if (passwordFile.fail())
 	{
 		cout << "Error opening file containing verified passwords." << endl;
-	}
+
+	}//end of if
 
 	//While not at the end of file (eof), user-entered username will be compared to verified usernames in the
 	//file "usernames.dat".
+	
 	while (!passwordFile.eof())
 	{
-		//Verified username is copied from username file and placed into compareCreds string.
+	
+		//Verified password is copied from password file and placed into compareCreds string.
 		getline(passwordFile, compareCreds);
-
-		//compareCreds string is compared to user-entered username. If two match, username is accepted. Else not accepted.
+		
+		//compareCreds string is compared to user-entered password. If two match, password is accepted. Else not accepted.
 		if (compareCreds == password)
 		{
-			passwordYesOrNo = 1;
-		}
+			passwordYesOrNo = true;
+			break;
+		}//end of if
 		else
 		{
-			passwordYesOrNo = 0;
-		}
-	}
+			passwordYesOrNo = false;
+			//continue;
+		}//end of else
+	}//end of while
+
 	//The file containing the verified passwords is closed
 	passwordFile.close();
+
 	//If both the username and password are verified, then the function returns 1. Else 0.
-	if ((usernameYesOrNo + passwordYesOrNo) == 2)
+	if (usernameYesOrNo == true && passwordYesOrNo == true)
 	{
 		return 1;
 	}
@@ -144,13 +179,69 @@ int Pharmacy::verifyCredentials(string username, string password)
 	{
 		return 0;
 	}
-
-	//The file containing the verified passwords is closed
-	passwordFile.close();
-	
 }
+
+
+void Pharmacy::displayPharmacistOptions()
+{
+
+	cout << "Welcome, " << username << "." << endl << endl;
+
+	do
+	{
+		cout << "What would you like to do?" << endl
+			<< "Enter [1] to fill a prescription." << endl
+			<< "Enter [2] to check the current stock of medications." << endl
+			<< "Enter [3] to update stock." << endl
+			<< "Enter [4] to display a list of receipts" << endl;
+
+		cin >> pharmacistDecision;
+
+		switch (pharmacistDecision)
+		{
+		case 1:
+			fillPrescription();
+			break;
+		case 2:
+			viewStock();
+			break;
+		case 3:
+			updateStock();
+			break;
+		case 4:
+			displayReceipts();
+			break;
+		}
+		
+		cout << "Would you like to do something else?" << endl
+			<< "Enter [1] if you would like to display the list of options again." << endl
+			<< "Enter [0] if you are done making decisions." << endl;
+
+		cin >> continueOrNot;
+	} while (continueOrNot);
+	
+
+}
+
+
+void Pharmacy::fillPrescription()
+{
+
+}
+
+
+void Pharmacy::updateStock()
+{
+
+}
+
 
 void Pharmacy::viewStock()
 {
 	//Provide code that opens the file containing stock and allows changes.
+}
+
+void Pharmacy::displayReceipts()
+{
+
 }
